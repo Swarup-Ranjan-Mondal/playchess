6d4729d0603bc7df0c5e3b54806109333a9f1815
Swarup Ranjan Mondal <swaranmon@gmail.com>
2025-04-25 11:20:22 +0530

    modularize Chess components & refine styles

--- frontend/src/components/ChessBoard/ChessBoard.js
+++ frontend/src/components/ChessBoard/ChessBoard.js
@@ -3,6 +3,54 @@ import "./ChessBoard.css";
 import { useSelector } from "react-redux";
 import ChessPiece from "../ChessPiece/ChessPiece";
 
+const getSquareId = (rank, file, ranks, files, reverse) => {
+  const col = String.fromCharCode(97 + (!reverse ? file : files - file - 1));
+  const row = !reverse ? ranks - rank : rank + 1;
+  return `${col}${row}`;
+};
+
+const getNotation = (rank, file, ranks, files, reverse) => {
+  const alphabet =
+    rank === ranks - 1
+      ? String.fromCharCode(97 + (!reverse ? file : files - file - 1))
+      : null;
+  const number = file === 0 ? (!reverse ? ranks - rank : rank + 1) : null;
+
+  return { alphabet, number };
+};
+
+const renderChessPieces = (
+  chessBoard,
+  ranks,
+  files,
+  handleMouseDown,
+  reverse
+) => {
+  if (!chessBoard) return null;
+
+  return chessBoard
+    .split("\n")
+    .map((rank, i) =>
+      rank
+        .split(" ")
+        .map(
+          (pieceSymbol, j) =>
+            pieceSymbol !== "." && (
+              <ChessPiece
+                key={`piece-${String.fromCharCode(97 + j)}${ranks - i}`}
+                pieceSymbol={pieceSymbol}
+                rank={ranks - i}
+                file={j + 1}
+                ranks={ranks}
+                files={files}
+                handleMouseDown={handleMouseDown}
+                reverse={reverse}
+              />
+            )
+        )
+    );
+};
+
 const ChessBoard = ({ ranks, files, handleMouseDown, reverse = false }) => {
   let { chessBoard } = useSelector((state) => state.gameDetails);
 
@@ -19,62 +67,39 @@ const ChessBoard = ({ ranks, files, handleMouseDown, reverse = false }) => {
       <div className="chess-board">
         {[...Array(ranks).keys()].map((rank) =>
           [...Array(files).keys()].map((file) => {
-            let color = (rank + file) % 2 === 0 ? "light" : "dark";
-            let length = color.length;
+            const color = (rank + file) % 2 === 0 ? "light" : "dark";
+            const squareId = getSquareId(rank, file, ranks, files, reverse);
+            const { alphabet, number } = getNotation(
+              rank,
+              file,
+              ranks,
+              files,
+              reverse
+            );
+            const backgroundImage = `/images/squares/square-${
+              color[0] + color[color.length - 1]
+            }.png`;
 
             return (
               <div
                 key={`${rank}${file}`}
-                id={`${String.fromCharCode(
-                  97 + (!reverse ? file : files - file - 1)
-                )}${!reverse ? ranks - rank : rank + 1}`}
+                id={squareId}
                 className={`square ${color}`}
-                style={{
-                  backgroundImage: `url(/images/squares/square-${
-                    color[0] + color[length - 1]
-                  }.png)`,
-                }}
+                style={{ backgroundImage: `url(${backgroundImage})` }}
               >
-                {rank === ranks - 1 && (
-                  <div className="notation alphabet">
-                    {`${String.fromCharCode(
-                      97 + (!reverse ? file : files - file - 1)
-                    )}`}
-                  </div>
+                {alphabet && (
+                  <div className="notation alphabet">{alphabet}</div>
                 )}
-
-                {file === 0 && (
-                  <div className="notation number">{`${
-                    !reverse ? ranks - rank : rank + 1
-                  }`}</div>
-                )}
-
+                {number && <div className="notation number">{number}</div>}
                 {rank === 0 &&
                   file === 0 &&
-                  chessBoard !== "" &&
-                  chessBoard
-                    .split("\n")
-                    .map((rank, i) =>
-                      rank
-                        .split(" ")
-                        .map(
-                          (pieceSymbol, j) =>
-                            pieceSymbol !== "." && (
-                              <ChessPiece
-                                key={`piece-${String.fromCharCode(97 + j)}${
-                                  ranks - i
-                                }`}
-                                pieceSymbol={pieceSymbol}
-                                rank={ranks - i}
-                                file={j + 1}
-                                ranks={ranks}
-                                files={files}
-                                handleMouseDown={handleMouseDown}
-                                reverse={reverse}
-                              />
-                            )
-                        )
-                    )}
+                  renderChessPieces(
+                    chessBoard,
+                    ranks,
+                    files,
+                    handleMouseDown,
+                    reverse
+                  )}
               </div>
             );
           })
--- frontend/src/components/ChessPiece/ChessPiece.js
+++ frontend/src/components/ChessPiece/ChessPiece.js
@@ -2,6 +2,45 @@ import React from "react";
 import "./ChessPiece.css";
 import { imageNameOfPieces } from "../../utils/helper";
 
+const getPieceId = (pieceSymbol, file, files, reverse) => {
+  if (pieceSymbol.toLowerCase() === "p") {
+    return `${pieceSymbol}${!reverse ? file : files - file + 1}`;
+  }
+
+  if (
+    pieceSymbol.toLowerCase() !== "k" &&
+    pieceSymbol.toLowerCase() !== "q"
+  ) {
+    return `${pieceSymbol}${
+      !reverse
+        ? Math.ceil((2 * file) / files)
+        : 3 - Math.ceil((2 * file) / files)
+    }`;
+  }
+
+  return pieceSymbol;
+};
+
+const getPieceClass = (pieceSymbol, file, rank, files, ranks, reverse) => {
+  const colorClass =
+    pieceSymbol.toUpperCase() === pieceSymbol ? "white" : "black";
+  const col = String.fromCharCode(
+    97 + (!reverse ? file - 1 : files - file)
+  );
+  const row = !reverse ? rank : ranks - rank + 1;
+  return `piece ${colorClass} ${col}${row}`;
+};
+
+const getPieceStyle = (pieceSymbol, rank, file, ranks) => {
+  return {
+    backgroundImage: `url(/images/pieces/${imageNameOfPieces[pieceSymbol]}.svg)`,
+    top: `${(ranks - rank) * 100}%`,
+    right: `-${(file - 1) * 100}%`,
+    bottom: `-${(ranks - rank) * 100}%`,
+    left: `${(file - 1) * 100}%`,
+  };
+};
+
 const ChessPiece = ({
   pieceSymbol,
   rank,
@@ -11,33 +50,15 @@ const ChessPiece = ({
   handleMouseDown,
   reverse = false,
 }) => {
-  const getPieceId = (pieceSymbol, file) => {
-    return pieceSymbol.toLowerCase() === "p"
-      ? `${pieceSymbol}${!reverse ? file : files - file + 1}`
-      : pieceSymbol.toLowerCase() !== "k" && pieceSymbol.toLowerCase() !== "q"
-      ? `${pieceSymbol}${
-          !reverse
-            ? Math.ceil((2 * file) / files)
-            : 3 - Math.ceil((2 * file) / files)
-        }`
-      : pieceSymbol;
-  };
+  const id = getPieceId(pieceSymbol, file, files, reverse);
+  const className = getPieceClass(pieceSymbol, file, rank, files, ranks, reverse);
+  const style = getPieceStyle(pieceSymbol, rank, file, ranks);
 
   return (
     <div
-      id={getPieceId(pieceSymbol, file)}
-      className={`piece ${
-        pieceSymbol.toUpperCase() === pieceSymbol ? "white" : "black"
-      } ${String.fromCharCode(97 + (!reverse ? file - 1 : files - file))}${
-        !reverse ? rank : ranks - rank + 1
-      }`}
-      style={{
-        backgroundImage: `url(/images/pieces/${imageNameOfPieces[pieceSymbol]}.svg)`,
-        top: `${(ranks - rank) * 100}%`,
-        right: `-${(file - 1) * 100}%`,
-        bottom: `-${(ranks - rank) * 100}%`,
-        left: `${(file - 1) * 100}%`,
-      }}
+      id={id}
+      className={className}
+      style={style}
       onMouseDown={handleMouseDown}
       onTouchStart={handleMouseDown}
     ></div>
